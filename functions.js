e263e8b8ba15aacf07c6201901e23223d7ae45c7
PeterBoss <cph-pt75@cphbusiness.dk>
2017-02-27 21:54:06 +0100

    day 1

---
+++ functions.js
@@ -0,0 +1,27 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var sub = function (n1, n2) {
+    return n1 - n2;
+};
+var cb = function (n1, n2, callback) {
+    if (typeof n1 === "number" && typeof n2 === "number" && typeof callback === "function") {
+        return "Result from the two numbers: " + n1 + "+" + n2 + "=" + callback(n1, n2);
+    } else {
+        try {
+            throw new Error('Whoops!');
+        } catch (e) {
+            console.log(e.name + ': ' + e.message);
+        }
+    }
+};
+
+console.log(add(1, 2))     // 3
+console.log(add)          // "function"
+console.log(add(1, 2, 3)); // 3, ignoring the third parameter?
+console.log(add(1));	  // 1 + nothing = ?
+console.log(cb(3, 3, add)); // the correct answer
+console.log(cb(4, 3, sub)); // "4 + 3 = 1" 
+console.log(cb(3, 3, add())); // an exception, "add()" would be the returned value when you call "add"? 
+console.log(cb(3, "hh", add));// concatenating 3 and hh
+
